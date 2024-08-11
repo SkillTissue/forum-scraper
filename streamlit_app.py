25efa07cfdea06ff1d3e4ec9aa4e0bf400a44bb7
SkillTissue <107127436+SkillTissue@users.noreply.github.com>
2024-08-11 22:21:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ options = Options()
 options.add_argument("--headless")  
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-driver = get_driver(
+driver = get_driver()
 
 try:
     driver.get("https://hypixel.net/online")
