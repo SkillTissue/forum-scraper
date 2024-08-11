4ed1787db414f61872370c673e5ae1d4d93f3113
SkillTissue <107127436+SkillTissue@users.noreply.github.com>
2024-08-11 22:27:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ driver = get_driver()
 
 st.write("Driver intialized")
 
-try:
+if True:
     st.write("trying hypixel")
     driver.get("https://hypixel.net/online")
     st.write("We got hypixel")
@@ -82,6 +82,3 @@ try:
                 print(f"sad error: {e}")
 
             time.sleep(5)
-
-finally:
-    driver.quit()
